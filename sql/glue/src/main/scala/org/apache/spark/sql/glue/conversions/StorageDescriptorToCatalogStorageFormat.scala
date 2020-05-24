658bd91a8786e33c525d13107f3126934018b9f4
edgar.klerks <edgar.klerks@sanoma.com>
2020-05-24 22:02:53 +0200

    added last needed converter

--- sql/glue/src/main/scala/org/apache/spark/sql/glue/conversions/StorageDescriptorToCatalogStorageFormat.scala
+++ sql/glue/src/main/scala/org/apache/spark/sql/glue/conversions/StorageDescriptorToCatalogStorageFormat.scala
@@ -1,8 +1,20 @@
 package org.apache.spark.sql.glue.conversions
 
+import java.net.URI
+
 import com.amazonaws.services.glue.model.StorageDescriptor
 import org.apache.spark.sql.catalyst.catalog.CatalogStorageFormat
+import scala.collection.JavaConverters._
 
 object StorageDescriptorToCatalogStorageFormat extends Conversion[StorageDescriptor, CatalogStorageFormat] {
-  override def apply(x: StorageDescriptor): CatalogStorageFormat = ???
-}
+  override def apply(storageDescriptor: StorageDescriptor): CatalogStorageFormat = {
+    new CatalogStorageFormat(
+      locationUri = Some(URI.create(storageDescriptor.getLocation)),
+      inputFormat = Some(storageDescriptor.getInputFormat),
+      outputFormat = Some(storageDescriptor.getOutputFormat),
+      serde = Some(storageDescriptor.getSerdeInfo.getSerializationLibrary),
+      compressed = storageDescriptor.getCompressed,
+      properties = storageDescriptor.getParameters.asScala.toMap
+    )
+  }
+ }
